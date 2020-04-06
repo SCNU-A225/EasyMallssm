6a95b2d3109d32243343dbe12bfb1479b1130702
JiangJian1998 <1197749258@qq.com>
2020-04-06 19:50:59 +0800

    更新readme

---
+++ EXAMPLE/easymall.sql
@@ -0,0 +1,117 @@
+CREATE DATABASE  IF NOT EXISTS `easymall` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `easymall`;
+-- MySQL dump 10.13  Distrib 5.7.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: easymall
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `pid` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `buyNum` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `orderitem`
+--
+
+DROP TABLE IF EXISTS `orderitem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orderitem` (
+  `order_id` varchar(100) NOT NULL DEFAULT '',
+  `product_id` varchar(100) NOT NULL DEFAULT '',
+  `buynum` int(11) DEFAULT NULL,
+  PRIMARY KEY (`order_id`,`product_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `pnum` int(11) DEFAULT NULL,
+  `imgurl` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  `nickname` varchar(100) DEFAULT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `id` varchar(100) NOT NULL,
+  `money` double DEFAULT NULL,
+  `receiverinfo` varchar(255) DEFAULT NULL,
+  `paystate` int(11) DEFAULT NULL,
+  `ordertime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `user_id` (`user_id`) USING BTREE,
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- Dump completed on 2020-04-06 19:29:21
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # EasyMallssm
-使用SSM框架搭建电子商务平台
+`EasyMallssm`使用SSM框架搭建电子商务平台，网页管理后台使用了X-Admin后台模板。含有登录/注册、购物车、订单、后台等模块。
+
+---
+
+# 示例图片
+[所有示例图片](./EXAMPLE/)
+
+![example photo](./EXAMPLE/10.png)
+
+![example photo](./EXAMPLE/1.png)
+
+![example photo](./EXAMPLE/4.png)
+
+![example photo](./EXAMPLE/5.png)
+
+![example photo](./EXAMPLE/3.png)
+
+![example photo](./EXAMPLE/2.png)
+
+![example photo](./EXAMPLE/6.png)
+
+![example photo](./EXAMPLE/7.png)
+
+![example photo](./EXAMPLE/8.png)
+
+
+---
+# 使用
+1. 使用git克隆或直接下载该项目
+2. 使用`Eclipse`等导入该Java项目
+3. 导入`EXAMPLE`文件夹中的`easymall.sql`数据库文件到`mysql`中
+3. 由于编译使用的Java版本可能不一致，如果提示错误请根据您电脑的环境修改项目配置
+4. 编译并运行
+
+## 改善该项目
+1. 提出[Issues](https://github.com/SCNU-A225/EasyMallssm/issues)，告知我们
+2. 创建[Pull requests](https://github.com/SCNU-A225/EasyMallssm/pulls)
+
+---
+# 更新
+暂无更新
+
+# 声明
+该项目是练习项目，没有参与任何商业行为。
+
+**若该项目侵害了您的权益请务必通知我们删除该项目！**
+
+# 许可
+[Apache License 2.0](https://github.com/SCNU-A225/EasyMallssm/blob/master/LICENSE)
+
